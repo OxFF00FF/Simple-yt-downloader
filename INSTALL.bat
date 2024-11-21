942705a462aab6f60c412b768705fa52ae84de74
nikita <wot_98_98@mail.ru>
2024-11-21 16:10:42 +0600

    - init -

--- INSTALL.bat
+++ INSTALL.bat
@@ -14,4 +14,10 @@ if not exist "venv" (
     echo Activating virtual environment...
     call venv\Scripts\activate
     echo.
+
+    set "shortcutPath=%cd%\Youtube Video Downloader.lnk"
+    set "targetPath=%cd%\Src\START_WT.bat"
+    set "iconPath=%cd%\Images\Icon.ico"
+
+    powershell "$ws = New-Object -COMObject WScript.Shell; $shortcut = $ws.CreateShortcut('%shortcutPath%'); $shortcut.TargetPath = '%targetPath%'; $shortcut.IconLocation = '%iconPath%'; $shortcut.Save()"
 )
